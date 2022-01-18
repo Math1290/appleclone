c8c95a2cefe4cc9c4e4bcbbed2d5b8d7c57aa10e
Math1290 <87650931+Math1290@users.noreply.github.com>
2022-01-18 16:39:05 -0300

    ajustes finais
    ajustes finais

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function lupa(){
+    document.querySelector('.pesquisa').style.display = 'block'
+    pesquisa.classList("animação-pesquisa")
+}
+
+function fecharlupa(){
+    document.querySelector('.pesquisa').style.display = 'none'
+}
